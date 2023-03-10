f8010fe51e9aaec06ee9c5c9e8dd53e25045899a
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-03-10 13:22:26 +0100

    done

--- src/server.js
+++ src/server.js
@@ -7,10 +7,32 @@ import {
   notfoundHandler,
   unauthorizedHandler,
 } from "./errorsHandler.js";
+import cors from "cors";
+import createHttpError from "http-errors";
 
 const server = Express();
 const port = process.env.PORT;
 
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+
+server.use(
+  cors({
+    origin: (currentOrigin, corsNext) => {
+      if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+        corsNext(null, true);
+      } else {
+        corsNext(
+          createHttpError(
+            400,
+            `origin ${currentOrigin} is not in the whitelist`
+          )
+        );
+      }
+    },
+  })
+);
+
+// server.use(Express.static(publicFolderPath));
 server.use(Express.json());
 
 server.use("/medias", mediasRouter);
